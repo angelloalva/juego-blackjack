8e1fb7bc59273d6ee31dc5a45215abd9cd4e437f
Mikel <mikelangel14@gmail.com>
2021-08-04 17:39:38 -0500

    se agrega la libreria underscore para barajar las cartas

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,3 +6,26 @@
  * h=corazones
  */
 let baraja=[];
+let tipos=['C','D','H','S'];
+let mayores=['J','Q','K','A'];
+let j=0;
+const crearbaraja = ()=>{
+    for(let i=2;i<=14;i++){     
+        if (i>=11) {
+            for (let tipo of tipos){
+                baraja.push( mayores[j] + tipo);
+           } 
+            j++;
+        }else{
+          for (let tipo of tipos){
+                baraja.push( i + tipo);
+            }  
+        }
+               
+    }
+    console.log(baraja);
+    baraja=_.shuffle(baraja);
+    console.log(baraja);
+};
+
+crearbaraja();
